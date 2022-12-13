ebb5c73591c24241472738449076d4f9aff75ef2
Avery B <94032280+AveryB88@users.noreply.github.com>
2022-12-12 23:18:10 -0600

    Update Study.js

--- src/Decks/Study.js
+++ src/Decks/Study.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
 import { readDeck } from "../utils/api/index";
-import CardList from "../Cards/CardLists"
+import CardList from "../Cards/CardLists";
 
 
 export default function Study() {
@@ -59,12 +59,8 @@ if (deck && deck.id && cards.length < 3) {
     <div>
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="breadcrumb-item">
-            <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
-          </li>
+          <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+          <li className="breadcrumb-item"><Link to={`/decks/${deck.id}`}>{deck.name}</Link></li>
           <li className="breadcrumb-item active" aria-current="page">Study</li>
         </ol>
       </nav>
